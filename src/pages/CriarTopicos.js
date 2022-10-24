48a6eee40879e04f13e1048f5987e0b0e9f0cb71
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-24 18:16:08 -0300

    create topics routes and insert into a database

--- src/pages/CriarTopicos.js
+++ src/pages/CriarTopicos.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React,  { useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from 'react-router-dom'
 
 import Navbar from "../components/Navbar";
 import LogoMenu from "../components/LogoMenu";
@@ -18,6 +20,33 @@ import SalvarBtn from "../components/Buttons/Salvar";
 import Input from "../components/Form/Input";
 
 function CriarTopicos() {
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const [ title, setTitle ] = useState();
+  const [ order, setOrder ] = useState();
+  const [ active_icon, setActiveIcon ] = useState();
+  const [ disabled_icon, setDisabledIcon ] = useState();
+
+    const createTopics = (event) => {
+      event.preventDefault();
+
+      axios.post(`http://localhost:80/createTopics/${params.skillId}`, {
+        title: title,
+        order, order,
+        active_icon: active_icon,
+        disabled_icon: disabled_icon
+
+      })
+      .then((response) => {
+        navigate(`/topicos/${params.skillId}`)
+      })
+      .catch((error) => {
+        navigate(`/criartopico/${params.skillId}`)
+    })
+  }
+
   return (
     <>
       <Navbar>
@@ -38,13 +67,13 @@ function CriarTopicos() {
           <BCLink href="#">Criar Tópico</BCLink>
         </Breadcrumbs>
 
-        <Form>
+        <Form onSubmit={createTopics} >
           <Titulo1>Criar Tópico</Titulo1>
 
-          <Input placeholder="Título" />
-          <Input  placeholder="Ordem" type="number" />
-          <Input  placeholder="Ícone Ativo" type="url" />
-          <Input  placeholder="Ícone Desativado"/>
+          <Input placeholder="Título" onChange={event => setTitle(event.currentTarget.value)} value={title}  />
+          <Input  placeholder="Ordem" type="number" onChange={event => setTitle(event.currentTarget.value)} value={title}  />
+          <Input  placeholder="Ícone Ativo" onChange={event => setActiveIcon(event.currentTarget.value)} value={active_icon} />
+          <Input  placeholder="Ícone Desativado" onChange={event => setActiveIcon(event.currentTarget.value)} value={active_icon}/>
 
           <SalvarBtn type="Submit">Salvar</SalvarBtn>
         </Form>        
