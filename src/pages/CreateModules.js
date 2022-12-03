462a348d317f892e83f011b01f4de655599d1cbe
Ana Freitas <ana.freitaas16@gmail.com>
2022-12-03 19:10:35 -0300

    refactoring: create modules - page name

--- src/pages/CreateModules.js
+++ src/pages/CreateModules.js
@@ -1,14 +1,12 @@
 import React,  { useState } from "react";
 import axios from "axios";
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom';
 
 import Navbar from "../components/Navbar";
 import LogoMenu from "../components/LogoMenu";
 import PublicarBtn from '../components/Buttons/Publicar';
-
 import BackBtn from '../components/Buttons/Back';
-import backIcon from '../icons/back.svg';
-import TooltipBack from "../components/Tooltip/TooltipBack";
+import { ReactComponent } from '../icons/back.svg';
 
 import Breadcrumbs from "../components/Breadcrumbs";
 import BCLink from "../components/Breadcrumbs/Link";
@@ -18,38 +16,40 @@ import Form from "../components/Form";
 import Titulo1 from "../components/Titulos/Titulo1";
 import SaveButton from "../components/Buttons/Save";
 import Input from "../components/Form/Input";
+import DescriptionText from "../components/Form/TextArea";
 
-function CriarSubtopicos() {
+function CreateModules() {
 
   const navigate = useNavigate();
   const params = useParams();
 
   const [ title, setTitle ] = useState();
   const [ order, setOrder ] = useState();
+  const [ description, setDescription ] = useState();
 
-  const createSubTopics = (event) => {
+
+  const createModules = (event) => {
     event.preventDefault();
 
-    axios.post(`http://localhost:80/createSubTopics/${params.topicsId}`, {
+    axios.post(`http://localhost:80/createModules/${params.subtopicsId}`, {
       title: title,
-      order: order  
+      order: order,
+      description: description
 
     })
     .then((response) => {
-      navigate(`/subtopicos/${params.topicsId}`)
+      navigate(`/objetosaprendizagem/${params.subtopicsId}`)
     })
     .catch((error) => {
-      navigate(`/criarsubtopico/${params.topicsId}`)
+      navigate(`/criarobjeto/${params.subtopicsId}`)
   })
 }
-
 
   return (
     <>
       <Navbar>
-        <BackBtn href="javascript:history.back()">
-          <img src={backIcon}/>
-          <TooltipBack className="tooltip">Voltar</TooltipBack>
+        <BackBtn href="javascript:javascript:history.go(-1)">
+          <ReactComponent />
         </BackBtn>
         
         <LogoMenu href="/skill"></LogoMenu>
@@ -58,24 +58,27 @@ function CriarSubtopicos() {
       </Navbar>
       
       <Caixa>
+
         <Breadcrumbs>
           <BCLink to={'/skill'}> Skills </BCLink>
-          <BCLink to={`/topicos/:skillId`}> Inteligência Emocional </BCLink>
+          <BCLink to={`/topicos/:skillId`} >Inteligência Emocional</BCLink>
           <BCLink to={`/subtopicos/:topicsId`} >Introdução</BCLink>
-          <BCLink href="#">Criar subtópico</BCLink>
+          <BCLink to={`/objetosaprendizagem/:subtopicsId`}>O que vamos tratar no módulo?</BCLink>
+          <BCLink>Criar Objeto de Aprendizagem</BCLink>
         </Breadcrumbs>
 
-        <Form onSubmit={createSubTopics} >
-          <Titulo1>Criar Subtópico</Titulo1>
+        <Form onSubmit={createModules} >
 
-          <Input placeholder="Título" onChange={event => setTitle(event.currentTarget.value)} value={title}  />
-          <Input  placeholder="Ordem" type="number" onChange={event => setOrder(event.currentTarget.value)} value={order} />
+          <Titulo1>Criar Objeto de Aprendizagem</Titulo1>
+          <Input placeholder="Título" onChange={event => setTitle(event.currentTarget.value)} value={title} />
+          <Input placeholder="Ordem" type="number"  onChange={event => setOrder(event.currentTarget.value)} value={order}/>
+          <DescriptionText placeholder="Descrição"  onChange={event => setDescription(event.currentTarget.value)} value={description} />
           <SaveButton type="Submit">Save</SaveButton>
-          
+
         </Form>        
       </Caixa>
     </>
   );
 }
 
-export default CriarSubtopicos;
+export default CreateModules;
