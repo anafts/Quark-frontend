731a292360c7887df362592dc13d498444d2a276
Ana Freitas <ana.freitaas16@gmail.com>
2022-12-03 19:36:29 -0300

    deleted pages and components

--- src/components/Buttons/DropdowConteudos.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const DropdownConteudos = styled.div `
-    visibility: hidden;
-    opacity: 0;
-    transition: 0.5s;
-`
-
-export default DropdownConteudos;
--- src/components/ObjetoDeAprendizagem/TituloConteudos.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import TituloListagens from '../Titulos/TituloListagens'
-
-
-const TituloConteudos = styled(TituloListagens) `
-    padding: 1.5vh 0 5px 0;
-`
-
-export default TituloConteudos;
