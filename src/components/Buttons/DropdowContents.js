68881573d486f8ccb9676a72a39dac1975531cf2
Ana Freitas <ana.freitaas16@gmail.com>
2022-12-03 19:34:47 -0300

    refactoring: component name

---
+++ src/components/Buttons/DropdowContents.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const DropdownContents = styled.div `
+    visibility: hidden;
+    opacity: 0;
+    transition: 0.5s;
+`
+
+export default DropdownContents;
---
+++ src/components/ObjetoDeAprendizagem/TituloContents.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import TituloListagens from '../Titulos/TituloListagens'
+
+
+const TituloContents = styled(TituloListagens) `
+    padding: 1.5vh 0 5px 0;
+`
+
+export default TituloContents;
