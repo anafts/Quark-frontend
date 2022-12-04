cf311cd49a36d9af8a5abd80ab8b7975d00ddb8e
Ana Freitas <ana.freitaas16@gmail.com>
2022-12-04 09:25:13 -0300

    remove: components and page

--- src/components/Form/InputArquivo.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const InputArquivo = styled.input `
-    display: none;
-`
-
-export default InputArquivo;
--- src/components/Form/NomeArquivo.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import Input from './Input'
-
-const NomeArquivo = styled(Input) `
-    width: 248px;
-`
-
-export default NomeArquivo;
